443971430d59d7b48096708d5324f2c6e6204e43
Aleksandra <aleksandra.skolasinska@gmail.com>
2023-04-14 17:42:06 +0200

    added docker, changed db and started to update the models (work in progress)

--- ticketsite/tickets/admin.py
+++ ticketsite/tickets/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
-from .models import Ticket
+from django.db import models
+from django.contrib.auth.models import User
+from .models import Ticket, TicketAssign
 
 
 class TicketAdmin(admin.ModelAdmin):
     date_hierarchy = 'publication_date'
+
+class TicketStaff(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+    def get_assigned_tickets(self):
+        return TicketAssign.objects.get_assigned_tickets(self.user)
+
+    def respond_to_ticket(self, ticket, message):
+        ticket.status = TicketStatus.to_do
+        ticket.save()
+        Ticket.objects.create(ticket=ticket, user=self.user, message=self.message)
+
 
 # Register your models here.
 admin.site.register(Ticket, TicketAdmin)
--- ticketsite/ticketsite/settings.py
+++ ticketsite/ticketsite/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'ticketsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_db_name',
+        'USER': 'my_db_user',
+        'PASSWORD': 'my_db_password',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
