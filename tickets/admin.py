9d3311d61ff0ec71a7b6ff747291497b6e8c7ceb
Aleksandra <aleksandra.skolasinska@gmail.com>
2023-05-31 23:08:16 +0200

    bug fixed: only staff can be assigneed to tickets

--- tickets/admin.py
+++ tickets/admin.py
@@ -10,17 +10,20 @@ class TicketReplyInline(admin.StackedInline):
 class TicketAdmin(admin.ModelAdmin):
     list_display = ('title', 'assignee', 'created_by', 'status', 'publication_date', 'update_date')
     inlines = [TicketReplyInline]
-    # readonly_fields = ['replies']
+    readonly_fields = ['created_by', 'status', 'publication_date', 'update_date']
+
 
     def save_model(self, request, obj, form, change):
         if not obj.pk:
             obj.created_by = request.user
         super().save_model(request, obj, form, change)
   
-    # def get_readonly_fields(self, request, obj=None):
-    #     if obj:
-    #         return self.readonly_fields + ['replies']
-    #     return self.readonly_fields
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            # Check if the user is staff/admin
+            if request.user.is_staff or request.user.is_superuser:
+                return self.readonly_fields + ['ticket_content']
+        return self.readonly_fields
     
 class TicketReplyAdmin(admin.ModelAdmin):
     list_display = ['ticket', 'user', 'timestamp']
@@ -31,6 +34,11 @@ class TicketReplyAdmin(admin.ModelAdmin):
             # If the logged-in user is not the author of the reply, make the 'message' field read-only
             return self.readonly_fields + ['message']
         return self.readonly_fields
+
+    def has_change_permission(self, request, obj=None):
+        if obj and obj.user != request.user:
+            return False
+        return super().has_change_permission(request, obj)
     
 
 class TicketStaff(models.Model):
@@ -46,6 +54,11 @@ class TicketStaff(models.Model):
         ticket.status = TicketStatus.to_do
         ticket.save()
         Ticket.objects.create(ticket=ticket, user=self.user, message=self.message)
+
+    def has_change_permission(self, request, obj=None):
+        if obj and obj.user != request.user:
+            return False
+        return super().has_change_permission(request, obj)
 
 
 # Register your models here.
